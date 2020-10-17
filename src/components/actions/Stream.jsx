886a431c68fa200cd1735cd16e220375d84e4006
JuaniGidoni <juan.gidoni@gmail.com>
2020-10-17 15:46:20 -0300

    Added no styled first front streaminfo data v-0.2

--- src/components/actions/Stream.jsx
+++ src/components/actions/Stream.jsx
@@ -5,6 +5,8 @@ const Stream = ({username, stream, streams, setStreams, id, setStreamsInfo, stre
     
     let alertbox = document.getElementById('alert');
     let alertBtn = document.createElement('p');
+
+    // Arrow function to delete stream
     const deleteStream = () => {
         setStreams(streams.filter((el) => el.id !== stream.id));
         alertBtn.innerText = 'Stream deleted...';
@@ -13,6 +15,11 @@ const Stream = ({username, stream, streams, setStreams, id, setStreamsInfo, stre
             alertbox.removeChild(alertBtn);
         }, 2000);
     }
+    // Function to capitalize first letter
+    function capitalizeFirstLetter(string) {
+        return string.charAt(0).toUpperCase() + string.slice(1);
+    }
+    // Async Arrow function to check stream data with try catch.
     const checkStream = async () => {
             try {
                 const response = await axios.get(`https://api.twitch.tv/helix/users?login=${username}`, {
@@ -51,7 +58,23 @@ const Stream = ({username, stream, streams, setStreams, id, setStreamsInfo, stre
             <li id={id}>{username}</li>
             <button onClick={checkStream} className={`${stream.checkstats ? "complete-btn" : "complete-btn text-white"}`} disabled={`${stream.checkstats ? "disabled" : ''}`}><i className="fas fa-chart-bar"></i></button>
             <button onClick={deleteStream} className="trash-btn"><i className="fas fa-trash"></i></button>
+            </div>            
+            {stream.checkstats ? 
+            <div className="stats">
+                <div className="d-flex flex-row">
+                <p className="userName-title mr-2">Username</p>
+                <p className="userName"> {`${capitalizeFirstLetter(stream.fullstats.data[0].display_name)}`}</p>
+                </div>
+                <div className="d-flex flex-row">
+                <p className="account-title mr-2">Account Type</p>
+                <p className="account-type"> {`${capitalizeFirstLetter(stream.fullstats.data[0].broadcaster_type)}`}</p>
+                </div>
+                <div className="d-flex flex-row">
+                <p className="viewers-title mr-2">Total Views</p>
+                <p className="viewersCount"> {`${stream.fullstats.data[0].view_count}`}</p>
+                </div>
             </div>
+            : ''}
         </div>
     );
 }
